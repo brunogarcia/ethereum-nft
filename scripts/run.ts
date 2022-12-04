55441eb4501891acd96fa589b8ad77242c10d4c0
Bruno Garcia <bruno.garcia@wallbox.com>
2022-12-04 16:20:03 +0100

    feat: create the nft contract

--- scripts/run.ts
+++ scripts/run.ts
@@ -5,6 +5,17 @@ const main = async () => {
   const nftContract = await nftContractFactory.deploy();
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
+
+  // Call the function.
+  let txn = await nftContract.makeAnEpicNFT()
+  // Wait for it to be mined.
+  await txn.wait()
+
+  // Mint another NFT for fun.
+  txn = await nftContract.makeAnEpicNFT()
+  // Wait for it to be mined.
+  await txn.wait()
+
 };
 
 const runMain = async () => {
@@ -17,4 +28,4 @@ const runMain = async () => {
   }
 };
 
-runMain();
+runMain()
